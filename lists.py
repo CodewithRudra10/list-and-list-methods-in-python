0f5a0bc1f392b9fc1a04e2b4daa983267fcade0d
Rudra <yadavrudra00010@gmail.com>
2025-11-07 22:46:13 +0530

    lists in python and it's methods

---
+++ lists.py
@@ -0,0 +1,32 @@
+# lst=[1,2,4,5 ,"rudra" ]
+# print(len(lst))
+# print(lst)
+# print(lst[1:5:2])
+
+# a=[7,0,8,0,0,9]
+# print(a.count(0))
+
+# l1=[4,67,89,34]
+# print(sum.l1)
+
+# list methods
+
+# l=[23,4,56,45,334]
+
+# print# l.reverse()
+# m=[35,67,98]
+# n=l+m
+# print(l.count(23))
+# print(l.add())
+
+# creating a dictionary
+
+
+# tuple methods
+
+t=("india","usa","uk","canada")
+temp=list(t)
+temp.append("brazil")
+t=tuple(temp)
+print(t)
+
